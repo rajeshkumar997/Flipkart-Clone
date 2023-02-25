74040ea805979ce9f62a41f330c8ca1935784c3e
Rajesh Kumar <rajekumar7589@gmail.com>
2023-02-25 10:59:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/header/Header';
+import Home from './components/home/Home';
+import { Box } from '@mui/material';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import DetailView from './details/DetailView';
+import Cart from './components/cart/Cart';
+import Payment from './components/Payment/Payment'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Box style={{ marginTop: 54 }}>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/product/:id' element={<DetailView />} />
+          <Route path='/Cart' element={<Cart />} />
+          <Route path='/Cart/Payment' element={<Payment />} />
+        </Routes>
+      </Box>
+    </BrowserRouter>
   );
 }
 
